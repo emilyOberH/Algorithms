96405d1b31fe6d5f61f93fb45ea0652dcc640d20
emilyOberH <36863559+emilyOberH@users.noreply.github.com>
2019-10-20 23:26:10 +0200

    Add files via upload

---
+++ Alg_1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+void display(int *, int);
+int partitionArr(int *, int, int *, int *, int);
+int quickSort(int *, int, int *, int *, int);
+
+int main(){
+    srand(time(NULL));
+    int n = 10;
+    int arr[10];
+
+    for(int i=0; i<10; ++i){
+        arr[i] = rand()%250;
+    }
+    display(arr, n);
+
+    int k = 0;
+    while(k<1||k>n){
+        cout<<"Looking for n-th largest number.\nEnter n: ";
+        cin>>k;
+    }
+
+    int first = 0;
+    int last = n-1;
+    int  g = quickSort(arr, k, &first, &last, n);
+
+    cout<<"The "<<k<<"-largest number in array is: "<<arr[g]<<endl;
+
+    //display(arr, n);
+
+    return 0;
+}
+
+void display(int *A, int n){
+    for(int i=0; i<n; ++i){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int quickSort(int *arr, int k, int *first, int *last, int n){
+    int g = 0;
+    while(((*last)!=(*first))||g<=2){
+        g = partitionArr(arr, k, first, last, n);
+    }
+    return g;
+}
+
+
+int partitionArr(int *arr, int k, int *first, int *last, int n){
+    int ind = n-k;
+
+    int i = (*first);
+    int pivot = rand() % ((*last)-(*first)+1) + (*first);
+    int tmp = arr[(*last)];
+    arr[(*last)] = arr[pivot];
+    arr[pivot] = tmp;
+
+    for(int j=(*first); j<(*last); ++j){
+            if(arr[j]<arr[(*last)]){
+                int tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+                ++i;
+            }
+    }
+    tmp = arr[i];
+    arr[i] = arr[(*last)];
+    arr[(*last)] = tmp;
+
+    if(i+1==ind){
+        (*last)=(*first);
+    }else if(i<ind){
+        (*first) = i+1;
+    }else if(i>ind){
+        (*last) = i-1;
+    }
+
+    return i+1;
+}
